cda2ff718ee8d96df022f6a889013bd9dca8d5a2
affar048 <affaraffu@gamil.com>
2025-05-24 17:41:12 +0530

    first commit

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// Set up EJS as the view engine
 app.set("view engine", "ejs");
 
-// Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// In-memory list to store todo items
 let items = [];
 
-// GET: Home page - show todo list
 app.get("/", (req, res) => {
     res.render("list", { ejes: items });
 });
 
-// POST: Add a new item
 app.post("/", (req, res) => {
     const item = req.body.ele1;
     if (item.trim() !== "") {
@@ -27,7 +22,6 @@ app.post("/", (req, res) => {
     res.redirect("/");
 });
 
-// POST: Delete an item by index
 app.post("/delete", (req, res) => {
     const index = req.body.index;
     if (index !== undefined && index >= 0 && index < items.length) {
@@ -36,7 +30,23 @@ app.post("/delete", (req, res) => {
     res.redirect("/");
 });
 
-// Start the server
+
+app.post("/edit", (req, res) => {
+    const index = req.body.index;
+    const newText = req.body.newText;
+
+    if (
+        index !== undefined &&
+        index >= 0 &&
+        index < items.length &&
+        newText.trim() !== ""
+    ) {
+        items[index] = newText.trim();
+    }
+    res.redirect("/");
+});
+
+
 app.listen(4000, () => {
     console.log("Server started on port 4000");
 });
