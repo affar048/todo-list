bb254cce5362e97b7b826bc6b94dc1e417c4903e
affar048 <affaraffu@gmail.com>
2025-06-02 20:17:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const app = express();
 
+const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
-mongoose.connect("mongodb+srv://affaraffu:LkkSO09DVxd6XFeH@todotest.0kbztty.mongodb.net/?retryWrites=true&w=majority&appName=todotest");
+mongoose.connect("mongodb+srv://affaraffu:LkkSO09DVxd6XFeH@todotest.0kbztty.mongodb.net/todotest");
 
-const Task = mongoose.model("Task", { name: String });
-app.get("/", async function(req, res) {
-  const tasks = await Task.find({});
-  res.render("list", { ejes: tasks });
+const Task = mongoose.model("Task", { 
+    name: String, 
+    priority: String 
 });
 
-app.post("/", async function(req, res) {
-  const taskName = req.body.ele1;
-  if (taskName && taskName.trim() !== "") {
-    await Task.create({ name: taskName });
-  }
-  res.redirect("/");
+app.get("/", async (req, res) => {
+    const tasks = await Task.find({});
+    res.render("list", { tasks });
 });
-app.put("/edit/:id", async function(req, res) {
-  const id = req.params.id;
-  const newText = req.body.newText;
-  await Task.findByIdAndUpdate(id, { name: newText });
-  res.redirect("/");
+
+app.post("/", async (req, res) => {
+    const { taskName, priority } = req.body;
+    if (taskName.trim()) {
+        await Task.create({ name: taskName, priority });
+    }
+    res.redirect("/");
 });
-app.delete("/delete/:id", async function(req, res) {
-  const id = req.params.id;
-  await Task.findByIdAndDelete(id);
-  res.redirect("/");
+
+app.put("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    await Task.findByIdAndUpdate(id, { name: req.body.newText });
+    res.redirect("/");
 });
-app.listen(8000, function() {
+
+app.delete("/delete/:id", async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.redirect("/");
+});
+
+app.listen(8000, function() { 
   console.log("Server is running");
 });
